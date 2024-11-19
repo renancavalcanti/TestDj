5ef0f6ddd25a893aa4bc4faa40521dcbb27d7f23
Renan Cavalcanti <renan.cavalcanti@evidentscientific.com>
2024-11-19 10:48:01 -0500

    config

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'renan',
+        'PASSWORD': '123456',
+        'HOST': 'database_host',
+        'PORT': '5432',
     }
 }
 
